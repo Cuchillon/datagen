71aec0cf0d485471cc176b5a9ce6e6b063cba46f
Cuchillon <ferickadmin@gmail.com>
2019-11-17 14:50:35 +0300

    Random performer

--- src/main/java/com/ferick/alexander/performers/RandomPerformer.java
+++ src/main/java/com/ferick/alexander/performers/RandomPerformer.java
@@ -1,8 +1,38 @@
 package com.ferick.alexander.performers;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class RandomPerformer {
 
     public String perform(final String[] args) {
-        return null;
+        if (args.length > 2) {
+            throw new IllegalStateException("Wrong number of arguments!");
+        }
+
+        String result = "";
+
+        switch (args[0]) {
+            case "alph":
+                result = RandomStringUtils.randomAlphabetic(Integer.parseInt(args[1]));
+                break;
+            case "alphnum":
+                result = RandomStringUtils.randomAlphanumeric(Integer.parseInt(args[1]));
+                break;
+            case "num":
+                result = RandomStringUtils.randomNumeric(Integer.parseInt(args[1]));
+                break;
+            case "double":
+                result = generateDouble(args[1]);
+                break;
+        }
+
+        return result;
+    }
+
+    private String generateDouble(String doubleLength) {
+        String[] lengthValues = doubleLength.split(".");
+        String intValue = RandomStringUtils.randomNumeric(Integer.parseInt(lengthValues[0]));
+        String fractionalValue = RandomStringUtils.randomNumeric(Integer.parseInt(lengthValues[1]));
+        return intValue + "." + fractionalValue;
     }
 }
